ff864500023e9b3131da9cb609a28722b90d267c
Александр Лунев <isuntc@gmail.com>
2016-02-26 23:04:40 +0800

    Add controlled props for set and change value

--- ScrollWheel/App.js
+++ ScrollWheel/App.js
@@ -1,12 +1,29 @@
-import React, { View, StyleSheet } from 'react-native';
+import React, { View, Text, StyleSheet } from 'react-native';
 import ScrollWheel from './ScrollWheel';
 
 export default class App extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: 0,
+    };
+  }
+
   render() {
+    const { value } = this.state;
+
     return (
       <View style={styles.container}>
-        <ScrollWheel />
+        <Text style={styles.value}>
+          {value}
+        </Text>
+        <ScrollWheel
+          value={value}
+          step={10}
+          onChange={value => this.setState({ value })}
+          />
       </View>
     );
   }
@@ -19,5 +36,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#EEE',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  value: {
+    fontSize: 72,
+    fontWeight: '100',
+    color: '#444',
+    marginBottom: 40,
+    marginTop: -80
   }
 })
--- ScrollWheel/ScrollWheel.js
+++ ScrollWheel/ScrollWheel.js
@@ -1,52 +1,65 @@
-import React, { View, StyleSheet, Animated, PanResponder } from 'react-native';
+import React, { View, StyleSheet, Animated, PanResponder, PropTypes } from 'react-native';
 
 export default class ScrollWheel extends React.Component {
 
+  static propTypes = {
+    value: PropTypes.number.isRequired,
+    step: PropTypes.number.isRequired,
+    onChange: PropTypes.func,
+  };
+
   constructor(props) {
     super(props);
+
     this.state = {
-      x: 0,
-      y: 0,
-      angle: new Animated.Value(0),
+      rotateAngle: new Animated.Value(0),
     };
     this.lastAngle = 0;
-
-    // this.state.angle.addListener(e => {
-    //   console.log(e);
-    // });
-    // console.log(this.state.angle);
+    this.inTouch = false;
 
     this._panResponder = PanResponder.create({
-			onStartShouldSetPanResponder: (evt, gestureState) => true,
-			onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
-			onMoveShouldSetPanResponder: (evt, gestureState) => true,
-			onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
-			onPanResponderGrant: (evt, gestureState) => {
-        const { x, y } = this.state;
+      onStartShouldSetPanResponder: (evt, gestureState) => true,
+      onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+      onMoveShouldSetPanResponder: (evt, gestureState) => true,
+      onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+      onPanResponderGrant: (evt, gestureState) => {
+        this.inTouch = true;
+        const { x, y, width, height } = this.state;
         const { x0, y0 } = gestureState;
-        const angleDeg = angleFromDots(x + 70, y + 70, x0, y0);
-        this.startAngle = angleDeg - this.lastAngle;
-			},
-			onPanResponderMove: (evt, gestureState) => {
-        const { x, y, angle } = this.state;
+        const angle = angleFromDots(x + width / 2, y + height / 2, x0, y0);
+        this.startAngle = angle - this.lastAngle;
+      },
+      onPanResponderMove: (evt, gestureState) => {
+        const { x, y, width, height, rotateAngle } = this.state;
+        const { value, step, onChange } = this.props;
         const { moveX, moveY } = gestureState;
-        const angleDeg = angleFromDots(x + 70, y + 70, moveX, moveY) - this.startAngle;
-        angle.setValue(angleDeg);
-        this.lastAngle = angleDeg;
-			},
-			onPanResponderTerminationRequest: (evt, gestureState) => true,
-			onPanResponderRelease: (evt, gestureState) => {
-        const { angle } = this.state;
-			},
-			onPanResponderTerminate: (evt, gestureState) => {
+        const angle = angleFromDots(x + width / 2, y + height / 2, moveX, moveY) - this.startAngle;
+        const delta = loopDelta((angle + this.startAngle) - (this.lastAngle + this.startAngle));
+        const newValue = value + Math.abs(delta) * step * (0 - Math.sign(delta));
 
-			},
-			onShouldBlockNativeResponder: (evt, gestureState) => true,
-		});
+        onChange && onChange(
+          newValue - newValue % this.props.step
+        );
+        rotateAngle.setValue(angle);
+        this.lastAngle = angle;
+      },
+      onPanResponderTerminationRequest: (evt, gestureState) => true,
+      onPanResponderRelease: (evt, gestureState) => {
+        this.inTouch = false;
+      },
+      onPanResponderTerminate: (evt, gestureState) => {
+        this.inTouch = false;
+      },
+      onShouldBlockNativeResponder: (evt, gestureState) => true,
+    });
+  }
+
+  shouldComponentUpdate() {
+    return !this.inTouch;
   }
 
   render() {
-    const { angle } = this.state;
+    const { rotateAngle } = this.state;
 
     return (
       <Animated.View
@@ -54,17 +67,14 @@ export default class ScrollWheel extends React.Component {
         onLayout={e => this.setState(e.nativeEvent.layout)}
         style={[styles.wheel, {
           transform: [{
-            rotate: angle.interpolate({
+            rotate: rotateAngle.interpolate({
               inputRange: [-180, 180],
-              outputRange: ['-180deg', '180deg'],
+              outputRange: ['180deg', '-180deg'],
             })
           }]
         }]}
         >
         <View style={styles.pit} />
-        <View style={[styles.pit, styles.pit1]} />
-        <View style={[styles.pit, styles.pit2]} />
-        <View style={[styles.pit, styles.pit3]} />
       </Animated.View>
     );
   }
@@ -73,34 +83,19 @@ export default class ScrollWheel extends React.Component {
 
 const styles = StyleSheet.create({
   wheel: {
-    width: 140,
-    height: 140,
-    borderRadius: 70,
+    width: 200,
+    height: 200,
+    borderRadius: 100,
     backgroundColor: '#FFF',
   },
   pit: {
     position: 'absolute',
-    top: 50,
-    left: 90,
+    top: 80,
+    left: 150,
     width: 40,
     height: 40,
     borderRadius: 20,
     backgroundColor: '#ccc',
-  },
-  pit1: {
-    top: 10,
-    left: 50,
-    backgroundColor: '#cf0',
-  },
-  pit2: {
-    left: 10,
-    top: 50,
-    backgroundColor: '#c0f',
-  },
-  pit3: {
-    top: 90,
-    left: 50,
-    backgroundColor: '#f0c',
   },
 });
 
@@ -108,5 +103,14 @@ function angleFromDots(x0, y0, x1, y1) {
   let theta = Math.atan2(y0 - y1, x1 - x0);
   if (theta < 0)
     theta += 2 * Math.PI;
-  return 0 - theta * (180 / Math.PI) + 360;
+  return theta * (180 / Math.PI);
+}
+
+function loopDelta(delta) {
+  const abs = Math.abs(delta);
+  if(abs > 300) {
+    return (360 - abs) * (0 - Math.sign(delta));
+  } else {
+    return delta;
+  }
 }
